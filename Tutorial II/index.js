1678b42e7be63dcacda85883efb858149bf7b9fb
danilocaverna <nq3i4fsx@hotmail.com>
2018-12-23 03:46:34 -0300

    node-npl pt

--- Tutorial II/index.js
+++ Tutorial II/index.js
@@ -1,26 +1,26 @@
 const { NlpManager } = require('node-nlp');
  
-const manager = new NlpManager({ languages: ['en'] });
+const manager = new NlpManager({ languages: ['pt'] });
 // Adds the utterances and intents for the NLP
-manager.addDocument('en', 'goodbye for now', 'greetings.bye');
-manager.addDocument('en', 'bye bye take care', 'greetings.bye');
-manager.addDocument('en', 'okay see you later', 'greetings.bye');
-manager.addDocument('en', 'bye for now', 'greetings.bye');
-manager.addDocument('en', 'i must go', 'greetings.bye');
-manager.addDocument('en', 'hello', 'greetings.hello');
-manager.addDocument('en', 'hi', 'greetings.hello');
-manager.addDocument('en', 'howdy', 'greetings.hello');
+manager.addDocument('pt', 'ate mais', 'greetings.bye');
+manager.addDocument('pt', 'tchau e se cuida', 'greetings.bye');
+manager.addDocument('pt', 'ta bom te vejo mais tarde', 'greetings.bye');
+manager.addDocument('pt', 'Ate logo', 'greetings.bye');
+manager.addDocument('pt', 'eu preciso ir', 'greetings.bye');
+manager.addDocument('pt', 'ola', 'greetings.hello');
+manager.addDocument('pt', 'oi', 'greetings.hello');
+manager.addDocument('pt', 'eae', 'greetings.hello');
  
 // Train also the NLG
-manager.addAnswer('en', 'greetings.bye', 'Till next time');
-manager.addAnswer('en', 'greetings.bye', 'see you soon!');
-manager.addAnswer('en', 'greetings.hello', 'Hey there!');
-manager.addAnswer('en', 'greetings.hello', 'Greetings!');
+manager.addAnswer('pt', 'greetings.bye', 'ate a proxima');
+manager.addAnswer('pt', 'greetings.bye', 'te vejo mais tarde!');
+manager.addAnswer('pt', 'greetings.hello', 'ola!');
+manager.addAnswer('pt', 'greetings.hello', 'saudacoes!');
  
 // Train and save the model.
 (async() => {
     await manager.train();
     manager.save();
-    const response = await manager.process('en', 'I have to go');
+    const response = await manager.process('en', 'eu tenho que ir');
     console.log(response);
 })();
